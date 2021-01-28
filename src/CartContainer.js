08870c36d908c692f046b5a0e86c8de8c4ff1ef2
sumonsheikh <sm.sumon117@gmail.com>
2021-01-28 09:46:56 +0600

    markup cart container

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-export default function CartContainer() {
+import CartItem from './CartItem';
+import {useGlobalContext} from './context';
+
+const CartContainer =()=> {
+    const {cart} =useGlobalContext();
+    if(cart.length === 0){
+        return(
+            <section className="cart">
+                <header>
+                    <h2>Your bag</h2>
+                    <h4 className="empty-cart">is currently empty</h4>
+                </header>
+            </section>
+        )
+    }
     return (
-        <div>
-            CartContainer
-        </div>
+        <section className="cart">
+            <header>
+                <h2>Your bag</h2>
+                <div>
+                    {cart.map((item) =>{
+                        return <CartItem key={item.id} {...item}/>
+                    })}
+                </div>
+            </header>
+            <footer >
+                <hr/>
+                <div className="cart-total">
+                    <h4>total <span>$0.00</span></h4>
+                </div>
+                <button className="btn clear-btn"
+                    onClick={()=>console.log("clear cart")}
+                >
+                    clear cart
+                </button>
+            </footer>
+        </section>
     )
 }
+export default CartContainer;
