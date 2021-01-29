6c5d2cefb13c64b87f2dd9eabeb46dca7ad6958e
sumonsheikh <sm.sumon117@gmail.com>
2021-01-29 18:16:06 +0600

    create conditionaly return in app component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import Navbar from './Navbar';
 import CartContainer from './CartContainer';
 
 function App() {
+  const { loading } = useGlobalContext();
+  if(loading){
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
+
     <main>
         <Navbar/>
         <CartContainer/>
